cce79aa5ec0649b639c4b0787a8908c4cbf9a9f8
Joseph O'Doherty <joseph.odoherty@gmail.com>
2016-08-23 17:07:27 -0700

    add getFloat and getDouble to lconf. change the interface to getInt (return bool so that failure can be detected

--- lib/lconf.cpp
+++ lib/lconf.cpp
@@ -124,11 +124,10 @@ error:
 	      name(), varName.c_str());
 	return false;
 }
-// returns 0 if there is an error
-int luaConf::getInt(string varName)
+// returns false if there is an error
+bool luaConf::getInt(string varName, int &i)
 {
 	size_t stack = 0;
-	int x;
 
 	char_separator<char> sep(".");
 	tokenizer<char_separator<char>> tokens(varName, sep);
@@ -140,7 +139,7 @@ int luaConf::getInt(string varName)
 
 	while (true) {
 		if (lua_isnumber(L, -1)) {
-			x = lua_tointeger(L, -1);
+			i = lua_tointeger(L, -1);
 			break;
 		} else if (lua_istable(L, -1)) {
 			lua_pushnil(L); // nil key
@@ -160,10 +159,62 @@ int luaConf::getInt(string varName)
 	}
 	lua_pop(L, stack);
 	debug("%s: loaded %s", name(), varName.c_str());
-	return x;
+	return true;
 error:
 	lua_pop(L, stack);
 	error("%s: variable '%s' is empty or does not exist",
 	      name(), varName.c_str());
-	return 0;
+	return false;
+}
+// returns true on success; false on error
+bool luaConf::getDouble(string varName, double &d)
+{
+	size_t stack = 0;
+
+	char_separator<char> sep(".");
+	tokenizer<char_separator<char>> tokens(varName, sep);
+
+	auto t = tokens.begin();
+
+	lua_getglobal(L, (*t).c_str());	// object to stack @ -1
+	stack++;
+
+	while (true) {
+		if (lua_isnumber(L, -1)) {
+			d = lua_tonumber(L, -1);
+			break;
+		} else if (lua_istable(L, -1)) {
+			lua_pushnil(L); // nil key
+			if (lua_next(L, -2)) {
+				lua_pop(L, 2); // pop off k,v
+				t++;
+				if (t != tokens.end()) {
+					lua_getfield(L, -1, (*t).c_str());
+					stack++;
+				}
+			} else {
+				goto error;
+			}
+		} else {
+			goto error;
+		}
+	}
+	lua_pop(L, stack);
+	debug("%s: loaded %s", name(), varName.c_str());
+	return true;
+error:
+	lua_pop(L, stack);
+	error("%s: variable '%s' is empty or does not exist",
+	      name(), varName.c_str());
+	return false;
+}
+// returns true on success; false on error
+bool luaConf::getFloat(string varName, float &f)
+{
+	if (getDouble(varName, (double&)f)) {
+		return true;
+	} else {
+		return false;
+	}
+
 }
--- lib/lconf.h
+++ lib/lconf.h
@@ -16,7 +16,9 @@ public:
 	virtual bool loadConf(const char *conf);
 	bool getString(string varName, string &varValue);
 	bool getBool(string varName);
-	int getInt(string varName);
+	bool getInt(string varName, int &i);
+	bool getDouble(string varName, double &d);
+	bool getFloat(string varName, float &f);
 protected:
 	lua_State *L;
 private:
