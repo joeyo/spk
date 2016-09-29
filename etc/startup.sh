36ced75680335964080a35e0f67ff785fbada04e
Joseph O'Doherty <joseph.odoherty@gmail.com>
2016-09-28 17:11:58 -0700

    update the example startup script

--- etc/startup.sh
+++ etc/startup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#af ipc:///tmp/broadband.zmq ipc:///tmp/af.zmq > /dev/null &
+# This is an example startup script that sets up a data processing pipeline
+# It shows a few examples of how to call the various spk programs with
+# tcp and ipc zmq sockets
 
+#af ipc:///tmp/broadband.zmq ipc:///tmp/af.zmq &
 #af2 1464840 ipc:///tmp/broadband.zmq ipc:///tmp/af2.zmq &
 
 #bp 300 5000 ipc:///tmp/subtr.zmq ipc:///tmp/bp.zmq &
@@ -9,6 +12,10 @@
 #notch ipc:///tmp/bp.zmq ipc:///tmp/notch.zmq &
 #noop ipc:///tmp/bp.zmq ipc:///tmp/noop.zmq &
 
+#bp 300 5000 tcp://localhost:2000 ipc:///tmp/bp.zmq &
+#subtr ipc:///tmp/bp.zmq tcp://localhost:3000 ipc:///tmp/subtr.zmq &
+#noop ipc:///tmp/subtr.zmq ipc:///tmp/noop.zmq &
+
 bp 300 5000 tcp://localhost:2000 ipc:///tmp/bp.zmq &
-subtr ipc:///tmp/bp.zmq tcp://localhost:3000 ipc:///tmp/subtr.zmq &
-noop ipc:///tmp/subtr.zmq ipc:///tmp/noop.zmq &
+noop ipc:///tmp/bp.zmq ipc:///tmp/noop.zmq &
+
